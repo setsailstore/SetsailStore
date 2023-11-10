ab8ec770be62759f34fc87ab5afba851c5487b9c
Vikoadrian <vikoadrian212@gmail.com>
2023-11-10 09:14:44 +0700

    a
    a

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const bar = document.getElementById("bar");
-const nav = document.getElementById("navbar");
-const close = document.getElementById("close");
 
-if(bar){
-    bar.addEventListener("click",()=>{
-        nav.classList.add("active")
-    })
+
+const navigasi = document.querySelector ('#navbar');
+const closemenu = document.querySelector ('.close');
+const openmenu = document.querySelector ('.mobile');
+
+openmenu.addEventListener('click',show);
+closemenu.addEventListener('click',close);
+
+function show(){
+    navigasi.style.display = 'flex';
+    navigasi.style.top = '0';
+    navigasi.style.transition =  "top 1s ease";
+
 }
-if(close){
-    close.addEventListener('click',()=>{
-        nav.classList.remove("active")
-    })
+
+function close(){
+    navigasi.style.top = '-100vh';
 }
 
